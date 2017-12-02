690d65079c452909e0b14dd92427c03ac7ea4dd6
Jeremy Shapiro <jeremynshapiro@gmail.com>
2017-12-02 00:15:50 -0800

    update

--- .laptop_configure.sh
+++ .laptop_configure.sh
@@ -25,7 +25,8 @@ brew bundle --global
 
 # install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | sudo bash -s stable --rails
+export rvm_ignore_dotfiles=yes
+\curl -sSL https://get.rvm.io | sudo bash -s stable --rails 
 
 # install latest gpgsuite
 LATEST_GPG_DMG=$(curl -s https://gpgtools.org/ | grep dmg | head -n1 | awk '{print $2}' | cut -d= -f2)
